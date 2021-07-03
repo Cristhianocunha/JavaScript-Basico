c9cd39d2af5a9f0e51151845b4fc691dee088ca0
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2021-07-03 20:11:23 -0300

    add

---
+++ Operadores.js
@@ -0,0 +1,56 @@
+// operadores artiméticos 
+
+// adição +
+var sumTwonNumbers = 1+3
+console.log(sumTwonNumbers)
+//subtração -
+var subTwonNumbers = 1-3
+console.log(subTwonNumbers)
+// multiplicação *
+var mulTwonNumbers = 2*3
+console.log(mulTwonNumbers)
+// divição /
+var divTwonNumbers = 2/2
+console.log(divTwonNumbers)
+// módulo ou resto de uma divição %
+console.log(8%3)
+// incremento ++
+var num = 1;
+console.log(++num)
+// decremento --
+var num2 = 2;
+console.log(--num2)
+//operação de atribuição
+// = atribuição
+
+
+// atribuição de soma
+var x = 1
+var y = 3
+
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
+
+// Operadores de comparação
+// ==
+// ===
+// !=
+// < ou >
+// > ou >=
+
+// Operadores Lógicos
+
+// e && -> AND
+// Ou || -> O
+
+ var num = 5;
+
+ if (num === 1){
+    console.log("num é igual a 1")
+}
+else {
+    console.log("num não é igual a 1")
+}
