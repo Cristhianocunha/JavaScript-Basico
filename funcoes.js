51f869023232a7a6dc491efb8938ed10cc112605
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2021-07-09 13:48:47 -0300

    funções

---
+++ funcoes.js
@@ -0,0 +1,10 @@
+function sayHello(name, lastName){
+    console.log('ola ' + name + ' '+ lastName)
+}
+
+sayHello("Silmara", "Conceiçõa");
+
+function sum(a, b){
+    return a + b;
+}
+console.log(sum(40, 60));
