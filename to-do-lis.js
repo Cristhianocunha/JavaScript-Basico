93f174b5d5920f015391537caaafb2d91236fc69
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2021-07-10 17:04:06 -0300

    atividade2

--- to-do-lis.js
+++ to-do-lis.js
@@ -2,25 +2,37 @@ const listContainer = document.querySelector('[data-list]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = ['html', 'js', 'css']
+let lists = []
 
-newListForm.addEventListener('submit', function(e) {
+newListForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    const listName = newListInput.Value 
+    const listName = newListInput.Value
     if (listName === null || listName === '') return
     const list = createList(listName)
     newListInput.Value = null
-    list.push(list)
+    lists.push(list)
     render()
 })
 
-function render()
-    lists.forEach(function(list){
+function createList(name){
+    return{id: Date.now().toString(), name: name }
+   }
+
+    function render(){
+        clearElement(listContainer)
+        lists.forEach(function (list) {
         const item = document.createElement('li')
         item.classList.add('item')
-        item.innerText = list
-        toDoContainer.appendChild(item)
+        item.innerText = list.name
+        listContainer.appendChild(item)
 
     })
+    }
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
 
-    
+
+
